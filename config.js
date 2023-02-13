41979f117cfcaec9664ef7905ff93648ef0f373a
karma-onyxbot <125288316+karma-onyxbot@users.noreply.github.com>
2023-02-13 16:34:27 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    app: {
+        px: '+',
+        token: '',
+        owners: ["1055889359334813847", "],
+        funny: '1055889359334813847',
+        color: '#2f3136',
+        footer: 'by Funny & Remade by karma_#0281 & NotSans#1870',
+        maxserver: '100',
+        maxVol: '150',
+        everyoneMention: false,
+        hostedBy: true,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+}
