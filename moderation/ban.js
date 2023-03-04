4de353c6f90c47d54bbc00158dde101af9327286
karma-onyxbot <125288316+karma-onyxbot@users.noreply.github.com>
2023-03-04 10:28:29 +0100

    Update ban.js

--- moderation/ban.js
+++ moderation/ban.js
@@ -34,7 +34,9 @@ module.exports = {
             if (member.id === message.author.id) {
                 return message.reply("Tu ne peux pas te bannir !")
             }
-    if (owner.get(`owners.${message.author.id}`) || config.app.owners.includes(message.author.id) || config.app.funny.includes(message.author.id) === true) return;
+
+            if (owner.get(`owners.${member.id}`) || config.app.owners.includes(member.id) || config.app.funny.includes(member.id) === true) return;
+   
 
             let reason = args.slice(1).join(" ") || `Aucune raison`
 
@@ -51,40 +53,8 @@ module.exports = {
             if (logchannel) logchannel.send({ embeds: [embed] }).catch(() => false)
         }
 
-        else if (message.member.roles.cache.has(p3.get(`perm3_${message.guild.id}`)) === true) {
-
-            let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-
-            if (!member) {
-                return message.reply("Merci de mentionner l'utilisateur que vous souhaitez bannir du serveur !")
-            }
-
-            if (member.id === message.author.id) {
-                return message.reply("Tu ne peux pas te bannir !")
-            }
-
-            if (member.roles.highest.position >= message.member.roles.highest.position || message.author.id !== message.guild.owner.id) {
-                return message.reply(`Vous ne pouvez pas ban un membre au dessus de vous`)
-            }
-
-            
-            if (owner.get(`owners.${message.author.id}`) || config.app.owners.includes(message.author.id) || config.app.funny.includes(message.author.id) === true)
-             return  message.reply("Tu ne peux pas le bannir !")
-             
-            let reason = args.slice(1).join(" ") || `Aucune raison`
-
-            message.reply({ content: `${member} à été banni du serveur` }).catch(err => err)
-            member.send({ content: `Tu as été banni par ${message.author} pour la raison suivante: \n\n ${reason}` })
-            member.ban({ reason: `${reason}` })
-
-            const embed = new Discord.MessageEmbed()
-                .setColor(color)
-                .setDescription(`<@${message.author.id}> a \`banni\` ${member} du serveur\nRaison : ${reason}`)
-                .setTimestamp()
-                .setFooter({ text: `📚` })
-                const logchannel = client.channels.cache.get(ml.get(`${message.guild.id}.modlog`))
-                if (logchannel) logchannel.send({ embeds: [embed] }).catch(() => false)
+       
 
         }
     }
-}
+
